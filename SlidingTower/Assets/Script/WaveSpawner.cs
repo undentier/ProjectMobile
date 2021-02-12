984e8e28b6a73ed1a727b9707201d602c190e435
Guillaume <g.roge@rubika-edu.com>
2021-02-12 14:47:22 +0100

    Add WaveSysteme

---
+++ SlidingTower/Assets/Script/WaveSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBfStart;
+    public float timeBtwWave;
+
+    [HideInInspector]
+    public float timeCounter;
+
+    private int waveIndex;
+
+
+    private void Start()
+    {
+        timeCounter = timeBfStart;
+    }
+
+    private void Update()
+    {
+        if (timeCounter <= 0f)
+        {
+            timeCounter = timeBtwWave;
+            StartCoroutine(SpawnWave());
+        }
+
+        timeCounter -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+    }
+}

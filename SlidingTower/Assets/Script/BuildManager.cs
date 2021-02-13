008d59e63e447bcc07fd0f02ef7a10f3ee82613a
Guillaume <g.roge@rubika-edu.com>
2021-02-13 19:12:19 +0100

    Build turret

---
+++ SlidingTower/Assets/Script/BuildManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    public GameObject basiqueTurretPrefab;
+
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        turretToBuild = basiqueTurretPrefab;
+    }
+}
---
+++ SlidingTower/Assets/Script/Node.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    private Color startColor;
+    public Vector3 buildOffSet;
+
+    private Renderer rend;
+    private GameObject turret;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Can't build there");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + buildOffSet, transform.rotation);
+    }
+
+
+
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}

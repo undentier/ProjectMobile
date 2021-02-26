f8049da61a4328b037ae12e0ef6b8fe777fdccfc
Guillaume <g.roge@rubika-edu.com>
2021-02-26 15:25:25 +0100

    Explosion mode done

--- SlidingTower/Assets/Script/Turret/Bullet.cs
+++ SlidingTower/Assets/Script/Turret/Bullet.cs
@@ -7,15 +7,25 @@ public class Bullet : MonoBehaviour
     [Header ("Stats")]
     public float speed;
     public int damage;
+    public float explosionRadius;
 
     [Header ("Effect")]
     public GameObject impactEffect;
 
-    private Transform target;
+    [Header("Unity SetUp")]
+    public string enemyLayerName;
 
+
+    private Transform target;
+    private LayerMask enemyLayerMask;
     public void GetTarget(Transform _target)
     {
         target = _target;
+    }
+
+    private void Start()
+    {
+        enemyLayerMask = LayerMask.GetMask(enemyLayerName);
     }
 
     void Update()
@@ -35,15 +45,23 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
 
     }
 
     void HitTarget()
     {
         GameObject effect = Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(effect, 2f);
+        Destroy(effect, 5f);
 
-        Damage(target);
+        if (explosionRadius > 0f)
+        {
+            Explosion();
+        }
+        else
+        {
+            Damage(target);
+        }
 
         Destroy(gameObject);
     }
@@ -52,5 +70,21 @@ public class Bullet : MonoBehaviour
     {
         Enemy e = enemy.GetComponent<Enemy>();
         e.TakeDamage(damage);
+    }
+
+    void Explosion()
+    {
+        Collider[] enemyInRange = Physics.OverlapSphere(transform.position, explosionRadius, enemyLayerMask);
+
+        foreach (Collider enemyIn in enemyInRange)
+        {
+            Damage(enemyIn.transform);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }

47a4a216f8db1de0d11684bbe7b26d0da4c30b19
Guillaume <g.roge@rubika-edu.com>
2021-03-30 16:46:21 +0200

    start

---
+++ SlidingTower/Assets/Script/Enemy/WaveSO.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "new wave", menuName = "Wave")]
+public class WaveSO : ScriptableObject
+{
+    public enum EnemyEnum {small, medium, big};
+
+    [Serializable]
+    public struct Enemy
+    {
+        public int number;
+        public EnemyEnum wichEnemy;
+        public float timeBeforeNextSpawn;
+    }
+
+    [Serializable]
+    public struct Wave
+    {
+        public Enemy[] enemies;
+    }
+
+    public Wave[] waves;
+}

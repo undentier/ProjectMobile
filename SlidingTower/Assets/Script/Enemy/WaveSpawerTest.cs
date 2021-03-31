a281a4f39b8451556b7137a6add136e589ca1a46
Guillaume <g.roge@rubika-edu.com>
2021-03-31 10:45:43 +0200

    start wave spawner

--- SlidingTower/Assets/Script/Enemy/WaveSpawerTest.cs
+++ SlidingTower/Assets/Script/Enemy/WaveSpawerTest.cs
@@ -5,19 +5,51 @@ using UnityEngine;
 public class WaveSpawerTest : MonoBehaviour
 {
     public float timeBeforeStartWave;
+    public int waveIndex;
+    public bool waveAlive;
 
     [Header ("Unity setup")]
     public GameObject smallEnemy;
     public GameObject mediumEnemy;
     public GameObject bigEnemy;
-
-    void Start()
-    {
-        
-    }
+    public WaveSO levelWaves;
+    public Transform spawnPoint;
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && !waveAlive)
+        {
+            StartCoroutine(SpawnWave());
+        }
+    }
 
+    IEnumerator SpawnWave()
+    {
+        waveAlive = true;
+        yield return new WaitForSeconds(timeBeforeStartWave);
+
+        for (int i = 0; i < levelWaves.waves[waveIndex].enemies.Length; i++)
+        {
+            GameObject enemyToSpawn = GetEnemyToSpawn(levelWaves.waves[waveIndex].enemies[i].wichEnemy);
+            Instantiate(enemyToSpawn, spawnPoint.position, spawnPoint.rotation);
+            yield return new WaitForSeconds(levelWaves.waves[waveIndex].enemies[i].timeBeforeNextSpawn);
+        }
+        waveAlive = false;
+        waveIndex++;
+    }
+
+    
+    private GameObject GetEnemyToSpawn(WaveSO.EnemyEnum wichEnemy)
+    {
+        switch (wichEnemy)
+        {
+            case WaveSO.EnemyEnum.small:
+                return smallEnemy;
+            case WaveSO.EnemyEnum.medium:
+                return mediumEnemy;
+            case WaveSO.EnemyEnum.big:
+                return bigEnemy;
+        }
+        return null;
     }
 }

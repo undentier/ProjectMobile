29f2497954cb4f82c396f21dacbf0bceb02399b9
Guillaume <g.roge@rubika-edu.com>
2021-04-11 16:51:26 +0200

    Base Menu

---
+++ SlidingTower/Assets/Script/UI/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseButton;
+    public GameObject pauseMenu;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+
+    public void PauseButton()
+    {
+        pauseMenu.SetActive(true);
+        pauseButton.SetActive(false);
+
+        Time.timeScale = 0f;
+    }
+    public void ResumeButton()
+    {
+        pauseMenu.SetActive(false);
+        pauseButton.SetActive(true);
+
+        Time.timeScale = 1f;
+    }
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu_Scene");
+    }
+}

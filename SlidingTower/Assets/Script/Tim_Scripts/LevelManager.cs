5d3101231e2d2d9be460f931d83a48aa2ba87877
Timothé Huerre <t.huerre@rubika-edu.com>
2021-04-16 19:06:26 +0200

    Menu fully working for all chapters but there still a display pb to fix

--- SlidingTower/Assets/Script/Tim_Scripts/LevelManager.cs
+++ SlidingTower/Assets/Script/Tim_Scripts/LevelManager.cs
@@ -5,19 +5,24 @@ using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject[] chapters;
-    public List<LevelsSO> levels;
+    public List<LevelsSO> levelsChapter1;
+    public List<LevelsSO> levelsChapter2;
+    public List<LevelsSO> levelsChapter3;
+    private List<LevelsSO> currentChapterLevels;
     int starTotal;
     public GameObject totalScoreText;
     public LevelDisplay levelButton;
     public RectTransform levelSelectionPanelTransform;
     public List<LevelDisplay> levelDisplays;
+    public Button chapter2Button;
+    public Button chapter3Button;
+    int chapterSelected;
     // Start is called before the first frame update
     void Start()
     {
         levelDisplays = new List<LevelDisplay>();
         StarNumber();
-        InitializeSelectionPanel();
+        //InitializeSelectionPanel();
     }
 
     // Update is called once per frame
@@ -28,14 +33,23 @@ public class LevelManager : MonoBehaviour
 
         }*/
             StarNumber();
+        UnlockChapters();
     }
 
     void StarNumber()
     {
         starTotal = 0;
-        for (int i = 0; i < levels.Count; i++)
+        for (int i = 0; i < levelsChapter1.Count; i++)
         {
-            starTotal += levels[i].score;
+            starTotal += levelsChapter1[i].score;
+        }
+        for (int j = 0; j < levelsChapter2.Count; j++)
+        {
+            starTotal += levelsChapter2[j].score;
+        }
+        for (int k = 0; k < levelsChapter3.Count; k++)
+        {
+            starTotal += levelsChapter3[k].score;
         }
         Write();
     }
@@ -43,19 +57,45 @@ public class LevelManager : MonoBehaviour
     {
         totalScoreText.GetComponent<Text>().text = "Nombre d'étoiles:" + starTotal.ToString();
     }
-    void InitializeSelectionPanel()
+    public void InitializeSelectionPanel(int chapter)
     {
-        for (int i = 0; i < levels.Count; i++)
+        levelDisplays.Clear();
+        for (int i = 0; i < levelSelectionPanelTransform.childCount; i++)
+        {
+            DestroyImmediate(levelSelectionPanelTransform.GetChild(0).transform);
+        }
+        chapterSelected = chapter;
+        switch (chapter)
+        {
+            case 0:
+                currentChapterLevels = levelsChapter1;
+                break;
+            case 1:
+                currentChapterLevels = levelsChapter2;
+                break;
+            case 2:
+                currentChapterLevels = levelsChapter3;
+                break;
+        }
+        for (int i = 0; i < currentChapterLevels.Count; i++)
         {
             LevelDisplay newleveldisplay = Instantiate(levelButton, levelSelectionPanelTransform);
-            newleveldisplay.level = levels[i];
+            newleveldisplay.level = currentChapterLevels[i];
             levelDisplays.Add(newleveldisplay);
         }
-        levelSelectionPanelTransform.sizeDelta = new Vector2((levels.Count-1)*(levelButton.GetComponent<RectTransform>().sizeDelta.x+levelSelectionPanelTransform.GetComponent<HorizontalLayoutGroup>().spacing), levelSelectionPanelTransform.sizeDelta.y);
+        levelSelectionPanelTransform.sizeDelta = new Vector2((currentChapterLevels.Count-1)*(levelButton.GetComponent<RectTransform>().sizeDelta.x+levelSelectionPanelTransform.GetComponent<HorizontalLayoutGroup>().spacing), levelSelectionPanelTransform.sizeDelta.y);
         levelSelectionPanelTransform.GetComponent<ScrollRect>().normalizedPosition = Vector2.zero;
     }
+    
     void UnlockChapters()
     {
-
+        if (starTotal > 7)
+        {
+            chapter2Button.interactable = true;
+            if (starTotal > 14)
+            {
+                chapter3Button.interactable = true;
+            }
+        }
     }
 }

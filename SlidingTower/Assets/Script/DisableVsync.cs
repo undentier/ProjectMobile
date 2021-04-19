09ee9d7d6c8b10463f80635c8a11af90a82cd480
Guillaume <g.roge@rubika-edu.com>
2021-04-19 12:09:03 +0200

    Start fix ui

---
+++ SlidingTower/Assets/Script/DisableVsync.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableVsync : MonoBehaviour
+{
+    void Start()
+    {
+        if (Application.isMobilePlatform)
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+    }
+}

5834d1a4494bd952934c4a75ff4fb309090d7a30
Timothé Huerre <t.huerre@rubika-edu.com>
2021-04-21 13:08:11 +0200

    AudioManager

---
+++ SlidingTower/Assets/Script/Sound/MusicManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+  //  public AudioClip ;
+
+
+    private bool canStartCoroutine = true;
+
+    private void Start()
+    {
+        audioSource.Play();
+    }
+    public void CrossfadeStart(AudioClip clip)
+    {
+        if (canStartCoroutine)
+        {
+            StartCoroutine(Crossfade(clip));
+        }
+    }
+    private IEnumerator Crossfade(AudioClip nextClip)
+    {
+        canStartCoroutine = false;
+        while (audioSource.volume > 0)
+        {
+            yield return new WaitForSeconds(0.1f);
+            audioSource.volume -= 0.1f;
+        }
+
+        audioSource.clip = nextClip;
+        audioSource.Play();
+
+        while (audioSource.volume < 1)
+        {
+            yield return new WaitForSeconds(0.1f);
+            audioSource.volume += 0.1f;
+        }
+        canStartCoroutine = true;
+    }
+}

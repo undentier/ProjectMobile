029f7b25cf536b8f9bd4386174220a25c109e826
Timothé Huerre <t.huerre@rubika-edu.com>
2021-05-30 17:55:10 +0200

    godmod implémenté

--- SlidingTower/Assets/Script/Tim_Scripts/LevelManager.cs
+++ SlidingTower/Assets/Script/Tim_Scripts/LevelManager.cs
@@ -21,6 +21,7 @@ public class LevelManager : MonoBehaviour
     int chapterSelected;
     public GameObject chapter2UnlockText;
     public GameObject chapter3UnlockText;
+    public bool godMode;
     // Start is called before the first frame update
     void Start()
     {
@@ -83,51 +84,66 @@ public class LevelManager : MonoBehaviour
             newleveldisplay.level = currentChapterLevels[i];
             levelDisplays.Add(newleveldisplay);
         }
-        levelSelectionPanelTransform.sizeDelta = new Vector2((currentChapterLevels.Count-1)*(levelButton.GetComponent<RectTransform>().sizeDelta.x+levelSelectionPanelTransform.GetComponent<HorizontalLayoutGroup>().spacing), levelSelectionPanelTransform.sizeDelta.y);
+        levelSelectionPanelTransform.sizeDelta = new Vector2((currentChapterLevels.Count - 1) * (levelButton.GetComponent<RectTransform>().sizeDelta.x + levelSelectionPanelTransform.GetComponent<HorizontalLayoutGroup>().spacing), levelSelectionPanelTransform.sizeDelta.y);
         levelSelectionPanelTransform.GetComponent<ScrollRect>().normalizedPosition = Vector2.zero;
     }
-  /*  public void ClearList()
-    {
-        levelDisplays.Clear();
-        for (int i = 0; i < levelSelectionPanelTransform.childCount; i++)
-        {
-            DestroyImmediate(levelSelectionPanelTransform.GetChild(0).gameObject);
-        }
-    }*/
-    
+    /*  public void ClearList()
+      {
+          levelDisplays.Clear();
+          for (int i = 0; i < levelSelectionPanelTransform.childCount; i++)
+          {
+              DestroyImmediate(levelSelectionPanelTransform.GetChild(0).gameObject);
+          }
+      }*/
+
     public void UnlockChapters()
     {
-        if (starTotal > 10)
+        if (godMode == false)
+        {
+            if (starTotal > 10)
+            {
+                chapter2Button.interactable = true;
+                chapter2Lock.SetActive(false);
+                chapter2UnlockText.SetActive(false);
+                if (starTotal > 16)
+                {
+                    chapter3Button.interactable = true;
+                    chapter3Lock.SetActive(false);
+                }
+                else if (starTotal <= 16)
+                {
+                    chapter3Button.interactable = false;
+                    chapter3UnlockText.GetComponent<Text>().text = "Stars required to unlock: " + (17 - starTotal);
+                    chapter3UnlockText.SetActive(true);
+                    chapter3Lock.SetActive(true);
+                }
+            }
+            else if (starTotal <= 10)
+            {
+                chapter2Button.interactable = false;
+                chapter2UnlockText.GetComponent<Text>().text = "Remaining Stars to unlock: " + (11 - starTotal);
+                chapter2Lock.SetActive(true);
+                chapter2UnlockText.SetActive(true);
+                if (starTotal <= 16)
+                {
+                    chapter3Button.interactable = false;
+                    chapter3UnlockText.GetComponent<Text>().text = "Remaining Stars to unlock: " + (17 - starTotal);
+                    chapter3UnlockText.SetActive(true);
+                    chapter3Lock.SetActive(true);
+                }
+            }
+        }
+        else
         {
             chapter2Button.interactable = true;
             chapter2Lock.SetActive(false);
-            chapter2UnlockText.SetActive(false);
-            if (starTotal > 16)
-            {
-                chapter3Button.interactable = true;
-                chapter3Lock.SetActive(false);
-            }
-            else if (starTotal <= 16)
-            {
-                chapter3Button.interactable = false;
-                chapter3UnlockText.GetComponent<Text>().text = "Stars required to unlock: " + (17 - starTotal);
-                chapter3UnlockText.SetActive(true);
-                chapter3Lock.SetActive(true);
-            }
+            chapter3Lock.SetActive(false);
+            chapter3Button.interactable = true;
         }
-        else if (starTotal <= 10)
-        {
-            chapter2Button.interactable = false;
-            chapter2UnlockText.GetComponent<Text>().text = "Remaining Stars to unlock: " + (11 - starTotal);
-            chapter2Lock.SetActive(true);
-            chapter2UnlockText.SetActive(true);
-            if (starTotal <= 16)
-            {
-                chapter3Button.interactable = false;
-                chapter3UnlockText.GetComponent<Text>().text = "Remaining Stars to unlock: " + (17 - starTotal);
-                chapter3UnlockText.SetActive(true);
-                chapter3Lock.SetActive(true);
-            }
-        }
+    }
+
+    public void ActiveGodMode()
+    {
+        godMode = true;
     }
 }
